de3de159aa7555c155b09f546b2762be0422d42b
Frasier <fsundra@gmail.com>
2024-03-04 23:45:42 +0800

    nice

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,27 +13,27 @@ const Hero = () => {
 			exit={{ opacity: 0, x: 500 }}
 			className="relative h-screen flex flex-col items-center justify-between md:justify-end pt-10 md:pb-8 px-4 overflow-hidden backgroundHero"
 		>
-			<div className="hidden absolute inset-0 md:flex flex-col z-0 mt-9">
+			<div className="hidden absolute inset-0 md:flex flex-col z-0 top-[8%]">
 				<div className="w-full relative flex justify-center m-4 group">
-					<div className=" relative p-2 rounded-full ">
-					<div className=" absolute inset-0 rounded-full bg-[linear-gradient(to_right,#0282f2,#ed4ff0)]"/>
-					<div className=" absolute inset-0 rounded-full blur-xl bg-[linear-gradient(to_right,#0282f2,#ed4ff0)]"/>
-                        <div className="w-full relative z-10 bg-black rounded-full px-4">
-                            <span className="absolute md:text-[6rem] uppercase text-transparent blur bg-clip-text bg-white text-center ">
-                                Good Vibes Only
-                            </span>
-                            <span className="md:text-[6rem] uppercase text-white text-center ">
-                                Good Vibes Only
-                            </span>
-                        </div>
+					<div className=" relative p-3 rounded-full ">
+						<div className=" absolute inset-0 rounded-full bg-[linear-gradient(to_right,#0282f2,#ed4ff0)]" />
+						<div className=" absolute inset-0 rounded-full blur-xl bg-[linear-gradient(to_right,#0282f2,#ed4ff0)]" />
+						<div className="w-full relative z-10 bg-black rounded-full px-8">
+							<span className="absolute md:text-[3rem] lg:text-[6rem] uppercase text-transparent blur bg-clip-text bg-white text-center font-bold" style={{wordSpacing: "5rem"}}>
+								Good Vibes Only
+							</span>
+							<span className="md:text-[3rem] lg:text-[6rem] uppercase text-white text-center font-bold" style={{wordSpacing: "5rem"}}>
+								Good Vibes Only
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="relative transform md:rotate-12 mb-2 z-20 mt-2 flex justify-center">
-				<span className="absolute text-[2rem] text-center md:text-[6rem] blur-xl uppercase font-extrabold hero-name-1 z-10 ">
+			<div className="relative transform md:rotate-12 z-20 py-4 mt-8 md:mt-0 flex justify-center">
+				<span className="absolute text-[2rem] md:text-[4rem] text-center lg:text-[6rem] blur-xl uppercase font-extrabold hero-name-1 z-10 ">
 					Jasmine Clarke
 				</span>
-				<span className="text-[2rem] text-center md:text-[6rem] uppercase font-extrabold hero-name-1 z-10 ">
+				<span className="text-[2rem] text-center md:text-[4rem] lg:text-[6rem] uppercase font-extrabold hero-name-1 z-10 px-6 md:px-12">
 					Jasmine Clarke
 				</span>
 			</div>
@@ -48,7 +48,7 @@ const Hero = () => {
 			<img
 				src={HeroImage}
 				alt="Jasmine and Seraphine"
-				className="block md:absolute md:z-10 md:top-0"
+				className="block md:absolute md:z-10 md:bottom-0"
 			/>
 		</motion.div>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,43 +1,64 @@
+import React from "react";
+import { motion } from "framer-motion";
 
-import React from 'react';
-import { motion } from 'framer-motion';
-const Navbar = ({ page, setPage, setHide} : { page: string, setPage : React.Dispatch<React.SetStateAction<string>>, setHide: React.Dispatch<React.SetStateAction<boolean>>}) => {
-
-    const handleNavigate = (name: string) => {
-        
-        if (name === page) return
-        
-        setHide(true)
-        setTimeout(() => {
-            setPage(name);
-        }, 450)
-        setTimeout(() => {
-            setHide(false);
-        }, 500)
+const LINKS = [
+    {
+        name: "Main",
+        screen: "Hero"
+    },
+    {
+        name: "Gallery",
+        screen: "Gallery"
     }
-    return (
+]
+const Navbar = ({
+	page,
+	setPage,
+	setHide,
+}: {
+	page: string;
+	setPage: React.Dispatch<React.SetStateAction<string>>;
+	setHide: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+	const handleNavigate = (name: string) => {
+		if (name === page) return;
 
-        <div className="fixed z-10 flex p-2 px-16 md:px-32 justify-center md:justify-between w-full gap-4 border-b border-white">
-            
-            <h3 className="font-bold uppercase hidden md:block"> Jasmine Clarke </h3>
-            <div className="flex flex-row gap-4">
+		setHide(true);
+		setTimeout(() => {
+			setPage(name);
+		}, 450);
+		setTimeout(() => {
+			setHide(false);
+		}, 500);
+	};
+	return (
+		<div className="fixed z-20 w-full pb-1">
+            <div className="absolute inset-0 bg-[linear-gradient(to_right,#0282f2,#ed4ff0)]"/>
+			<div className="relative bg-black z-10 flex p-2 px-16 md:px-32 justify-center md:justify-between items-center w-full gap-4">
+				<h3 className="font-bold uppercase hidden md:block">
+					{" "}
+					Jasmine Clarke{" "}
+				</h3>
+				<div className="relative z-50 flex flex-row gap-4">
+                    {LINKS.map((link, idx) => (
+					<motion.button
 
-                <motion.button 
-                whileHover={{ y:-3 }}
-                whileTap={{ y:-3 }}
-                onClick={() => handleNavigate("Hero")}>
-                    Main
-                </motion.button>
-                <motion.button 
-                whileHover={{ y:-3 }}
-                whileTap={{ y:-3 }}
-                onClick={() => handleNavigate("Gallery")}>
-                    Gallery
-                </motion.button>
-            </div>
-
-        </div>
-    );
+						whileHover={{ y: -3 }}
+						whileTap={{ y: -3 }}
+						onClick={() => handleNavigate(link.screen)}
+                        key={idx}
+                        
+					>
+                        <div className={`py-2 px-3 rounded-full transition-all ease-in-out duration-500 ${page === link.screen ? 'bg-[#0282f2]': 'bg-[#ed4ff0]'}`}>
+                            {link.name}
+                        </div>
+					</motion.button>
+                    
+                   ))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
